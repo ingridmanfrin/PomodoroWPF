d238140421258c1a7640e9f156330ebfeb4618fb
ingridmanfrin <ingrid.caldeira@itlab.com.br>
2023-01-28 01:06:55 -0300

    Ativação dos botões Pomodoro, Pausa Curta e Pausa Longa

--- PomodoroWPF/MainWindow.xaml.cs
+++ PomodoroWPF/MainWindow.xaml.cs
@@ -30,5 +30,61 @@ namespace PomodoroWPF
 
         private void Sample2_DialogHost_OnDialogClosing(object sender, DialogClosingEventArgs eventArgs)
             => Debug.WriteLine($"SAMPLE 2: Closing dialog with parameter: {eventArgs.Parameter ?? string.Empty}");
+
+        private void MudarCorBtnSelecionado(Button btn)
+        {
+            btn.Foreground = (SolidColorBrush)new BrushConverter().ConvertFrom("#DDFFFFFF");
+            btn.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#7F989797");
+            btn.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#7F989797");
+        }
+
+        private void MudarCorBtnNaoSelecionado(Button btn)
+        {
+            btn.Foreground = (SolidColorBrush)new BrushConverter().ConvertFrom("#7FFFFFFF");
+            btn.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#7FD8D8D8");
+            btn.BorderBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#7FD8D8D8");
+        }
+
+        private void MudarCorTema(string background, string retangulo)
+        {
+            GridBackground.Background = (SolidColorBrush)new BrushConverter().ConvertFrom(background);
+            RetanguloSuperior.Fill = (SolidColorBrush)new BrushConverter().ConvertFrom(retangulo);
+            RetanguloInferior.Fill = (SolidColorBrush)new BrushConverter().ConvertFrom(retangulo);
+        }
+
+
+        private void BtnPomodoro_Click(object sender, RoutedEventArgs e)
+        {
+            MudarCorTema("#FFFBC54F", "#FFFFD16C");
+
+            MudarCorBtnSelecionado(BtnPomodoro);
+            MudarCorBtnNaoSelecionado(BtnPausaCurta);
+            MudarCorBtnNaoSelecionado(BtnPausaLonga);
+
+            LabelTimer.Content = "25:00";
+        }
+
+        private void BtnPausaCurta_Click(object sender, RoutedEventArgs e)
+        {
+            MudarCorTema("#FF4BB999", "#FF6EBFA7");
+
+            MudarCorBtnSelecionado(BtnPausaCurta);
+            MudarCorBtnNaoSelecionado(BtnPomodoro);
+            MudarCorBtnNaoSelecionado(BtnPausaLonga);
+
+            LabelTimer.Content = "05:00";
+
+        }
+
+        private void BtnPausaLonga_Click(object sender, RoutedEventArgs e)
+        {
+            MudarCorTema("#FF50BCE0", "#FF73C4E0");
+
+            MudarCorBtnSelecionado(BtnPausaLonga);
+            MudarCorBtnNaoSelecionado(BtnPomodoro);
+            MudarCorBtnNaoSelecionado(BtnPausaCurta);
+
+            LabelTimer.Content = "15:00";
+        }
     }
 }
