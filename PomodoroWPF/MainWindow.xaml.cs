26d4e143e1608893e4f36e510f568d549a50e75d
ingridmanfrin <ingrid.caldeira@itlab.com.br>
2023-02-06 15:33:46 -0300

    Adicionado o cronômetro

--- PomodoroWPF/MainWindow.xaml.cs
+++ PomodoroWPF/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Windows.Threading;
 
 namespace PomodoroWPF
 {
@@ -23,13 +23,69 @@ namespace PomodoroWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer dispatcherTimer = new DispatcherTimer(); // DispatcherTimer: é o tipo da variável dispapatcherTimer
+        Stopwatch stopWatch = new Stopwatch();
+        int metaEmMinutos = 1; //versão final colocar 25, ao invés de 1
+
         public MainWindow()
         {
             InitializeComponent();
+            dispatcherTimer.Tick += new EventHandler(TimerTick); //Tick: é uma propriedade(caracteristica) do DispatcherTimer ..EventHandler:quem manipula o evento..(TimerTick): aqui eu referenciei minha classe TimerTick
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+        }
+
+        void TimerTick(object sender, EventArgs e)
+        {
+            if (stopWatch.IsRunning)
+            {
+                TimeSpan tempoPercorrido = stopWatch.Elapsed;  //stopWatch.Elapsed: tempo decorrido ou percorrido
+                TimeSpan tempoTotal = new TimeSpan(0, 0, metaEmMinutos, 0, 0); //Transformar metaEmMinutos em minutos em tipo TimeSpan
+                TimeSpan tempoRestante = tempoTotal - tempoPercorrido; //Subtrair um TimeSpan do outro TimeSpan
+                string tempoRestanteString = String.Format("{0:00}:{1:00}:{2:00}", tempoRestante.Minutes, tempoRestante.Seconds, tempoRestante.Milliseconds / 10);
+
+                LabelTimer.Content = tempoRestanteString;
+
+                if (tempoPercorrido.Seconds >= metaEmMinutos) // >= para obrigar a entrar nesse if e parar o cronometro, mesmo se o valor for maior (em caso de erro no pc, ou alguma outra coisa)
+                {
+                    if (stopWatch.IsRunning)
+                    {
+                        stopWatch.Stop();
+                    }
+                }
+            }
+        }
+        private void BtnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            stopWatch.Start();
+            dispatcherTimer.Start();
+        }
+
+        private void BtnPausar_Click(object sender, RoutedEventArgs e)
+        {
+            if (stopWatch.IsRunning)
+            {
+                stopWatch.Stop();
+            }
+
+        }
+
+        private void BtnResetar_Click(object sender, RoutedEventArgs e)
+        {
+            Resetar();
+
         }
 
         private void Sample2_DialogHost_OnDialogClosing(object sender, DialogClosingEventArgs eventArgs)
             => Debug.WriteLine($"SAMPLE 2: Closing dialog with parameter: {eventArgs.Parameter ?? string.Empty}");
+
+        private void Resetar()
+        {
+            stopWatch.Reset();
+
+            TimeSpan tempoTotal = new TimeSpan(0, 0, metaEmMinutos, 0, 0);
+            string tempoRestanteString = String.Format("{0:00}:{1:00}:{2:00}", tempoTotal.Minutes, tempoTotal.Seconds, tempoTotal.Milliseconds / 10);
+            LabelTimer.Content = tempoRestanteString;
+        }
 
         private void MudarCorBtnSelecionado(Button btn)
         {
@@ -61,7 +117,8 @@ namespace PomodoroWPF
             MudarCorBtnNaoSelecionado(BtnPausaCurta);
             MudarCorBtnNaoSelecionado(BtnPausaLonga);
 
-            LabelTimer.Content = "25:00";
+            metaEmMinutos = 25;
+            Resetar();
         }
 
         private void BtnPausaCurta_Click(object sender, RoutedEventArgs e)
@@ -72,8 +129,8 @@ namespace PomodoroWPF
             MudarCorBtnNaoSelecionado(BtnPomodoro);
             MudarCorBtnNaoSelecionado(BtnPausaLonga);
 
-            LabelTimer.Content = "05:00";
-
+            metaEmMinutos = 5;
+            Resetar();
         }
 
         private void BtnPausaLonga_Click(object sender, RoutedEventArgs e)
@@ -84,7 +141,10 @@ namespace PomodoroWPF
             MudarCorBtnNaoSelecionado(BtnPomodoro);
             MudarCorBtnNaoSelecionado(BtnPausaCurta);
 
-            LabelTimer.Content = "15:00";
+            metaEmMinutos = 15;
+            Resetar();
         }
+
+
     }
 }
